b1a12964928c9c0ab4ccbfec2cbfddc8d1228c7f
Juan Jose Ceballos Navia <juanfix@gmail.com>
2023-05-15 19:10:37 -0500

    social network added to Footer

--- src/ui/components/Footer.jsx
+++ src/ui/components/Footer.jsx
@@ -2,7 +2,25 @@ export const Footer = () => {
   return (
     <footer className="bg-transparent text-center text-lg-start">
       <div className="text-center text-muted p-3">
-        © 2023 Copyright ONE-Alura. Desarrollado por Juan José Ceballos Navia
+        <p>
+          © 2023 Copyright ONE-Alura. Desarrollado por Juan José Ceballos Navia.
+        </p>
+        <a
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://www.linkedin.com/in/juan-jose-ceballos-navia"
+          className="me-1"
+        >
+          <i className="fa-brands fa-linkedin fa-xl"></i>
+        </a>
+        <a
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://github.com/juanfix"
+          className="ms-1"
+        >
+          <i class="fa-brands fa-square-github fa-xl"></i>
+        </a>
       </div>
     </footer>
   );
